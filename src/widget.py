02caee1b5dbcbcff1b89edded45300d1454fbab2
Ilya Yaushkin <mexic2284@mail.ru>
2024-07-31 22:03:30 +0300

    add widget.py

---
+++ src/widget.py
@@ -0,0 +1,37 @@
+def mask_account_card(account_or_card: str) -> str:
+    """Функция, которая возвращает строку с замаскированным номером.
+    Для карт и счетов использует разные типы маскировки."""
+    # Определяем, является ли входная строка номером карты или счета
+    if account_or_card.lower().startswith("visa") or account_or_card.lower().startswith("mastercard"):
+        # Маскировка для карт
+        parts = account_or_card.split()
+        card_number = parts[-1]
+        masked_number = f"{card_number[:4]} {card_number[4:6]}** **** {card_number[-4:]}"
+        return " ".join(parts[:-1]) + " " + masked_number
+    elif account_or_card.lower().startswith("счет"):
+        # Маскировка для счетов
+        parts = account_or_card.split()
+        account_number = parts[-1]
+        masked_number = f"**{account_number[-4:]}"
+        return " ".join(parts[:-1]) + " " + masked_number
+
+
+####################################################################
+
+
+def get_date(date_str: str) -> str:
+    """Функция, которая преобразовывает строку даты
+    из '2024-03-11T02:26:18.671407' в формат '11.03.2024'"""
+    # Разделяем дату и время
+    date_part = date_str.split("T")[0]
+
+    # Разделяем дату на год, месяц, день
+    year, month, day = date_part.split("-")
+
+    # Форматируем дату как: "день, месяц, год"
+    formatted_date = f"{day}.{month}.{year}"
+
+    return formatted_date
+
+
+#######################################################################
