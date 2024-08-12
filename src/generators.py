b3e23add1b3f0f7123e77b56a0f803ceb5455824
Ilya Yaushkin <mexic2284@mail.ru>
2024-08-12 21:48:32 +0300

    make 3 function

---
+++ src/generators.py
@@ -0,0 +1,29 @@
+from typing import Dict, Iterator, List
+
+
+def filter_by_currency(transactions: list, currency: str) -> Iterator[Dict]:
+    """Функция, которая фильтрует список транзакций по заданной валюте"""
+    for transaction in transactions:
+        if transaction.get("operationAmount", {}).get("currency", {}).get("code") == currency:
+            yield transaction
+
+
+def transaction_descriptions(list_of_transactions: List[Dict]) -> Iterator[str]:
+    """Генерирует описание каждой транзакции по очереди"""
+    for operation in list_of_transactions:
+        description = operation.get("description")
+        if description:
+            yield description
+
+
+def card_number_generator(start: int, end: int) -> Iterator[str]:
+    """
+    Генерирует номера банковских карт в формате XXXX XXXX XXXX XXXX в заданном диапазоне.
+    """
+    for number in range(start, end + 1):
+        # Преобразуем число в строку
+        number_str = str(number)
+        # Создаем 16-значную строку, добавляя нули в начале
+        full_number = "0" * (16 - len(number_str)) + number_str
+        # Форматируем строку в виде XXXX XXXX XXXX XXXX
+        yield f"{full_number[:4]} {full_number[4:8]} {full_number[8:12]} {full_number[12:]}"
