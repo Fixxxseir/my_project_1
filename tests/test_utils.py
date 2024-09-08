bfbfcc97a8e6f71006a2fa6d904ff58387cf8c69
Ilya Yaushkin <mexic2284@mail.ru>
2024-09-08 12:54:26 +0300

    fixes

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,7 +1,7 @@
 import unittest
 import unittest.mock as mock
 
-from src.utils import get_transactions
+from src.utils import get_transactions_json
 
 
 class TestGetTransactions(unittest.TestCase):
@@ -9,27 +9,27 @@ class TestGetTransactions(unittest.TestCase):
     def test_file_not_exists(self) -> None:
         with mock.patch("os.path.exists") as mock_exists:
             mock_exists.return_value = False
-            self.assertEqual(get_transactions("nonexistent_file.json"), [])
+            self.assertEqual(get_transactions_json("nonexistent_file.json"), [])
 
     def test_file_not_list(self) -> None:
         with open("test_file.json", "w") as f:
             f.write('{"key": "value"}')
-        self.assertEqual(get_transactions("test_file.json"), [])
+        self.assertEqual(get_transactions_json("test_file.json"), [])
 
     def test_valid_file(self) -> None:
         with open("test_file.json", "w") as f:
             f.write('[{"key": "value"}]')
-        self.assertEqual(get_transactions("test_file.json"), [{"key": "value"}])
+        self.assertEqual(get_transactions_json("test_file.json"), [{"key": "value"}])
 
     def test_file_empty(self) -> None:
         with open("test_file.json", "w"):
             pass
-        self.assertEqual(get_transactions("test_file.json"), [])
+        self.assertEqual(get_transactions_json("test_file.json"), [])
 
     def test_file_corrupted(self) -> None:
         with open("test_file.json", "w") as f:
             f.write('{["key": "value"]')
-        result = get_transactions("test_file.json")
+        result = get_transactions_json("test_file.json")
         self.assertEqual(result, [])
 
 
